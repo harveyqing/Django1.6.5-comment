1b7711e4a9d1131411cfc4796276dc881bb59920
qingtian <harveyqing@gmail.com>
2014-09-01 12:44:21 +0800

    importlib: understand Django import mechanisms

--- django/utils/importlib.py
+++ django/utils/importlib.py
@@ -22,20 +22,23 @@ if six.PY3:
 else:
     def import_module(name, package=None):
         """Import a module.
+        导入一个模块。
 
         The 'package' argument is required when performing a relative import. It
         specifies the package to use as the anchor point from which to resolve the
         relative import to an absolute import.
+        当利用相对路径导入时，`package`参数是必须的。它指定包作为锚点使用，从这个锚点将相对导入
+        解析为绝对导入。
 
         """
-        if name.startswith('.'):
+        if name.startswith('.'):  #: 以点`.`开头的是相对导入
             if not package:
                 raise TypeError("relative imports require the 'package' argument")
-            level = 0
+            level = 0  #: 记录模块的层级，level = 0表示当前目录
             for character in name:
-                if character != '.':
+                if character != '.':  #: 遇到点号就表示层级加一
                     break
                 level += 1
             name = _resolve_name(name[level:], package, level)
-        __import__(name)
+        __import__(name)  #: 绝对导入的话就直接用`__import__` builtin导入
         return sys.modules[name]
